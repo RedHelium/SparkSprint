d8487808d0ed99cb234449249eb890f6258e91a3
Brilev Denis <brilevdv@gazdv.ru>
2024-06-18 11:04:09 +1000

    added task description, limit tasks for columns

--- STATIC/js/tasks.js
+++ STATIC/js/tasks.js
@@ -1,4 +1,20 @@
+// import {getCookie} from './tools.js';
+
 $(document).ready( function() {
+
+
+    $(".state-description-button").click( function() {
+        var container = $(this).closest(".task-container");
+        var description = container.find(".task-description");
+        var isDescHidden = description.is(":hidden");
+
+        if (isDescHidden == false) {
+            description.hide();
+        } else {
+            description.show();
+        }
+
+    });
 
     $("textarea").each(function () {
         this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
@@ -16,6 +32,8 @@ $(document).ready( function() {
         var task = input.closest(".task-container").attr('id-task');
         var column = input.closest(".task-container").attr('id-column');
         var name = input.val();
+        var description = input.closest(".task-container").find(".task-description").val();
+
 
         $.ajax({
             url: "tasks/update",
@@ -24,7 +42,7 @@ $(document).ready( function() {
                   'task': task,
                   'column': column,
                   'name': name,
-                  'description': '', //TODO done it
+                  'description': description,
                   'csrfmiddlewaretoken': getCookie('csrftoken'),
                 },            
             success: function(data) {
@@ -32,6 +50,33 @@ $(document).ready( function() {
             });
     }, 300); 
     });
+
+
+    $('textarea.task-description').on('keyup', function(e) {
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            var input = $(e.currentTarget);
+            var task = input.closest(".task-container").attr('id-task');
+            var column = input.closest(".task-container").attr('id-column');
+            var name = input.closest(".task-container").find(".task-name").val();
+            var description = input.val();
+    
+    
+            $.ajax({
+                url: "tasks/update",
+                type: 'POST',
+                data:  {
+                      'task': task,
+                      'column': column,
+                      'name': name,
+                      'description': description,
+                      'csrfmiddlewaretoken': getCookie('csrftoken'),
+                    },            
+                success: function(data) {
+                },
+                });
+        }, 300); 
+        });
 
 
 
@@ -85,6 +130,8 @@ $(document).ready( function() {
         var task = $(this).closest(".task-container").attr('id-task');
         var column = $(this).attr('id-column');
         var name = $(this).closest(".task-container").find(".task-name").val();
+        var description = $(this).closest(".task-container").find(".task-description").val();
+
         $.ajax({
             url: "tasks/update",
             type: 'POST',
@@ -92,7 +139,7 @@ $(document).ready( function() {
                     'task': task,
                     'column': column,
                     'name': name,
-                    'description': '', //TODO done it
+                    'description': description,
                     'csrfmiddlewaretoken': getCookie('csrftoken'),
                 },            
             success: function(data) {
@@ -100,22 +147,5 @@ $(document).ready( function() {
             },
             });
     });
-    
-
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
 
   });
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -11,7 +11,14 @@ def add_task(request):
     
     column = request.POST.get('column')
     
-    Task.objects.create(name='New task', column=TaskColumn.objects.get(id=column))
+    col = TaskColumn.objects.get(id=column)
+    max_tasks = col.max_task
+    current_tasks = Task.objects.filter(column=column).count()
+    
+    if current_tasks < max_tasks:
+        Task.objects.create(name='New task', column=col)
+    else:
+        print('Max limit!') #TODO replace on message
     
     return redirect(home)
 
@@ -24,10 +31,20 @@ def update_task(request):
     column = request.POST.get('column')
     
     taskObj = Task.objects.get(id=task)
+    col = TaskColumn.objects.get(id=column)
+    
+    if taskObj.column != col:
+        max_tasks = col.max_task
+        current_tasks = Task.objects.filter(column=column).count()
+        
+        if current_tasks < max_tasks:
+            taskObj.column = col
+        else:
+            print("Max limit!") #TODO replace on message
+        
     
     taskObj.name = name
     taskObj.description = description
-    taskObj.column = TaskColumn.objects.get(id=column)
     
     taskObj.save()
     
